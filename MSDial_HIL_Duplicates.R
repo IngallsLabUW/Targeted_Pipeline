30b9731d5265b0a461071138c5ff5d3e578f27e7
Regina Lionheart <regina@lionhe.art>
2020-01-23 09:54:35 -0800

    Add first half of BMIS to Control.Rmd, through HILIC duplicate check.

---
+++ MSDial_HIL_Duplicates.R
@@ -0,0 +1,19 @@
+#
+HILICS.duplicates <- IdentifyDuplicates(QCd.data)
+
+if (exists("HILICS.duplicates") == TRUE) {
+  duplicates.testing <- QCd.data %>%
+    filter(Metabolite.Name %in% HILICS.duplicates$Metabolite.Name) %>%
+    group_by(Metabolite.Name, Column) %>%
+    summarize(mean(Area.with.QC, na.rm = TRUE))
+
+  } else {
+  print("Non-HILIC data: no duplicates to detect.")
+}
+
+if (exists("duplicates.testing") == TRUE) {
+  QCd.data <- QCd.data %>%
+    filter(!(Metabolite.Name %in% HILICS.duplicates$Metabolite.Name & Column == "HILICNeg"))
+} else {
+  print("Non-HILIC data: no duplicates to remove.")
+}
