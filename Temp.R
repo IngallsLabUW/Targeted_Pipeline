ce20f4e5684c547588059390bd716eecfc7f3175
Regina Lionheart <regina@lionhe.art>
2020-08-21 10:15:32 -0700

    Add color lines to Temp.R graph.

--- Temp.R
+++ Temp.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 
-QCd <- read.csv("data_processed/Skyline_QE_QC_Output_Vitamins_2020-08-18.csv", 
+mytheme <- theme(panel.grid.major = element_line(colour="black", size = (0.03)),
+                 panel.grid.minor = element_line(size = (0.2), colour="grey"))
+
+QCd <- read.csv("data_processed/Skyline_QE_QC_Output_Vitamins_2020-08-19.csv", 
                 stringsAsFactors = FALSE) %>%
   slice(-1:-6) %>%
   select(-c(Description, Value)) %>%
@@ -8,12 +11,21 @@ QCd <- read.csv("data_processed/Skyline_QE_QC_Output_Vitamins_2020-08-18.csv",
   filter(!str_detect(Replicate.Name, "Blk|Std")) %>%
   mutate(Replicate.Name = Replicate.Name %>%
            str_replace("-",".")) %>%
-  # filter(!Precursor.Ion.Name == "B2-IS") %>%
-  mutate(Nutrient.Addition = ifelse(str_detect(Replicate.Name, "DMB"), "DMB_Added",
-                            ifelse(str_detect(Replicate.Name, "WBT"), "B12_Added", NA)))
+  filter(!str_detect(Replicate.Name, 
+                     "TruePoo|CultureMED4|Sept29QC|TruePooWeek1|TruePooWeek2|TruePooWeek3|TruePooWeek4|DSW700m|Process|Std")) %>%
+  separate(Replicate.Name, into = c("Date", "runtype", "Supergroup", "replicate"), remove = FALSE) %>%
+  mutate(Control.Status = ifelse(str_detect(Supergroup, "T0"),
+                                 "Incubation", ifelse(str_detect(Supergroup, "DSW"), "DeepSeaWater", 
+                                                      ifelse(str_detect(Supergroup, "Control"), "Control", "Treatments")))) %>%
+  mutate(Treatment.Status = ifelse(Control.Status == "Control", "Control",
+                                   ifelse(Control.Status == "DeepSeaWater", "DeepSeaWater",
+                                          ifelse(Control.Status == "Incubation", "TimeZero",
+                                                 ifelse(str_detect(Supergroup, "DMBnoBT"), "DMBnoB12",
+                                                        ifelse(str_detect(Supergroup, "WBT"), "B12",
+                                                               ifelse(str_detect(Supergroup, "DMB"), "DMB", "noB12"))))))) %>%
+  select(Replicate.Name, Supergroup, Precursor.Ion.Name, Area.with.QC, Treatment.Status)
 
-mytheme <- theme(panel.grid.major = element_line(colour="black", size = (0.03)),
-                 panel.grid.minor = element_line(size = (0.2), colour="grey"))
+
 
 all.plot <- ggplot(QCd, aes(x=Replicate.Name, y=Area, fill = Nutrient.Addition)) +
   facet_wrap(~Precursor.Ion.Name, scales = "free") +
@@ -38,6 +50,15 @@ NonQCd.plot <- ggplot(NonQCd, aes(x=Replicate.Name, y=Area, fill=Nutrient.Additi
   ggtitle("Vitamins: B12 Incubation, ONLY nonQC PEAKS") +
   theme(axis.text.x = element_blank())
 print(NonQCd.plot + mytheme)
+
+
+
+all.treatments.plot <- ggplot(all.treatments, aes(x=Replicate.Name, y=Area.with.QC, fill=Treatment.Status)) +
+  facet_wrap(~Precursor.Ion.Name, scales = "free") +
+  geom_bar(stat = "identity") +
+  ggtitle("Vitamins: B12 Incubation. All Treatments.") +
+  theme(axis.text.x = element_blank())
+print(all.treatments.plot + mytheme)
 
 print(paste("Total number of peaks:", length(QCd$Area)))
 print(paste("Number of NAs thrown out due to area minimum (less than 5000):", sum(is.na(QCd$Area.with.QC))))
