248e09b02458cd4a9253b52e2292ec90e8fa70e3
Regina Lionheart <regina@lionhe.art>
2020-12-11 07:51:18 -0800

    Compound Namecheck added to rearrange step.

--- src/Skyline_Rearrange.R
+++ src/Skyline_Rearrange.R
@@ -1,10 +1,10 @@
-## Skyline Rearrange
+## Skyline Rearrange and Compound Name Check
 
 csvFileName <- paste("data_intermediate/", software.pattern, "_combined_", file.pattern, "_", currentDate, ".csv", sep = "")
 
 if (matching.pattern == "pos|neg") {
-  replace_nonvalues <- function(x) (gsub("#N/A", NA, x))
   
+  replace_nonvalues <- function(x) (gsub("#N/A", NA, x))
   
   skyline.HILIC.pos <- skyline.HILIC.pos %>%
     mutate(Column = "HILICpos")
@@ -18,7 +18,7 @@ if (matching.pattern == "pos|neg") {
   
   skyline.classes.changed <- ChangeClasses(combined.skyline, start.column = 3, end.column = 7) 
 
-  write.csv(skyline.classes.changed, csvFileName, row.names = FALSE)
+  #write.csv(skyline.classes.changed, csvFileName, row.names = FALSE)
   
 } else {
   
@@ -26,7 +26,45 @@ if (matching.pattern == "pos|neg") {
   skyline.RP.Cyano <- skyline.classes.changed
   rm(skyline.classes.changed)
   
-  write.csv(skyline.RP.Cyano, csvFileName, row.names = FALSE)
+  #write.csv(skyline.RP.Cyano, csvFileName, row.names = FALSE)
+  
+}
+
+# Check compatibility and replace names -----------------------------------
+
+# Import standards sheet
+original.file <- combined.skyline
+
+Ingalls.Standards <- read.csv("https://raw.githubusercontent.com/IngallsLabUW/Ingalls_Standards/master/Ingalls_Lab_Standards_NEW.csv",
+                              stringsAsFactors = FALSE, header = TRUE) %>%
+  select(Compound.Name, Compound.Name_old) %>%
+  unique()
+
+
+if ("Precursor.Ion.Name" %in% colnames(original.file)) {
+  print("Checking for updated compound names.")
+  
+  compound.names <- original.file %>%
+    select(Precursor.Ion.Name) %>%
+    unique() %>%
+    rename(Compound.Name_old = Precursor.Ion.Name)
+  
+  combined.names <- compound.names %>%
+    left_join(Ingalls.Standards) %>%
+    rename(Precursor.Ion.Name = Compound.Name_old)
+  
+  combined.skyline <- combined.names %>%
+    left_join(original.file) %>%
+    mutate(Compound.Name = ifelse(is.na(Compound.Name), Precursor.Ion.Name, Compound.Name)) %>%
+    select(-Precursor.Ion.Name) %>%
+    rename(Precursor.Ion.Name = Compound.Name)
+  print("Compound names updated. Exporting file to data_intermediate.")
+  
+  #write.csv(combined.skyline, csvFileName, row.names = FALSE)
+  
+} else {
+  
+  stop("Your column is incorrectly named.") 
   
 }
 
